1748a33389278dc80ef979e65dc14c1cac8971ea
jylee9937 <lkj49864@ajou.ac.kr>
2022-03-28 14:56:10 +0900

    카메라 동작

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import "./App.css";
+import ImageClassifier from "./ImageClassifier";
+
+function App() {
+  return (
+    <div>
+      <ImageClassifier />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/ImageClassifier.js
@@ -0,0 +1,50 @@
+import React from "react";
+import * as mobilenet from "@tensorflow-models/mobilenet";
+import * as tf from "@tensorflow/tfjs";
+
+function ImageClassifier() {
+  let net;
+  const camera = React.useRef();
+  const figures = React.useRef();
+  const webcomElement = camera.current;
+
+  const run = async () => {
+    net = await mobilenet.load();
+
+    const webcam = await tf.data.webcam(webcomElement, {
+      resizeWidth: 220,
+      resizeHeight: 227,
+    });
+
+    while (true) {
+      const img = await webcam.capture();
+      const result = await net.classify(img);
+
+      if (figures.current) {
+        figures.current.innerText = `prediction : ${result[0].className} \n probability: ${result[0].probability}`;
+      }
+      img.dispose();
+      await tf.nextFrame();
+    }
+  };
+
+  React.useEffect(() => {
+    run();
+  });
+
+  return (
+    <>
+      <div ref={figures}></div>
+      <video
+        autoPlay
+        playsInline
+        muted={true}
+        ref={camera}
+        width="900"
+        height="600"
+      />
+    </>
+  );
+}
+
+export default ImageClassifier;
